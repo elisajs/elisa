6b0bb140497e135fd16e1c76c4449e263c28b250
Raúl G. González <raulggonzalez@justojs.org>
2016-05-02 12:38:37 +0200

    Fix #8

--- dist/es5/nodejs/elisa/lib/sync.js
+++ dist/es5/nodejs/elisa/lib/sync.js
@@ -4,23 +4,4 @@
 
 
 function (fn) {
-  var deasync = require("deasync");
-  var result;
-
-
-  deasync(function (done) {
-    function mydone(err, res) {
-      if (err) {
-        if (err instanceof Error) done(err);else 
-        done(new Error(err));} else 
-      {
-        result = res;
-        done();}}
-
-
-
-    fn(mydone);})();
-
-
-
-  return result;};
+  return require("justo-sync")(fn);};
